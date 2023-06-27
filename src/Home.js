d0862047992d7d44001c53093ef70951b4dbb8c1
Dnnguyen <dnnguyen100@gmail.com>
2023-06-27 01:00:50 -0400

    using filter to filter out props

--- src/Home.js
+++ src/Home.js
@@ -26,6 +26,10 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title={"All blogs"} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title={"Mario's blog"}
+      />
     </div>
   );
 };
