2f10342137ca269276cb2bbc45cff2cd902048ae
Dnnguyen <dnnguyen100@gmail.com>
2023-06-26 22:29:13 -0400

    click event

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
       <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="">Home</a>
-        <a href="">Contact</a>
+        <a href="">New Blog</a>
       </div>
     </nav>
   );
