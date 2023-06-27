e6c9e073911a5257205b670a705fefbd6d46d859
Dnnguyen <dnnguyen100@gmail.com>
2023-06-26 21:58:45 -0400

    initialized all files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  const person = {
+    name: "yoshi",
+    age: 30,
+  };
+
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App Component</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
