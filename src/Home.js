232e01a216e0b0bc02ec4b4c84c3d7149c0bd1aa
Dnnguyen <dnnguyen100@gmail.com>
2023-06-28 13:22:01 -0400

    run local json server, fetch data and handle loading screen

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,8 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My new website",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 1,
-    },
-    {
-      title: "Welcome to the party!",
-      body: "lorem ipsum...",
-      author: "yoshi",
-      id: 2,
-    },
-    {
-      title: "Web deb top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-
-  const [name, setName] = useState("mario");
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIspending] = useState(true);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -31,15 +11,27 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log("name updated!");
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:3000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+        setIspending(false);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title={"All blogs"} handleDelete={handleDelete} />
-      <button onClick={() => setName("luigi")}>change name</button>
-      <p>{name}</p>
+      {isPending && <div>Loading...</div>}
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title={"All blogs"}
+          handleDelete={handleDelete}
+        />
+      )}
     </div>
   );
 };
