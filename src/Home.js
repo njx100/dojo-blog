4d9849947743aa58553374076417167a35087aca
Dnnguyen <dnnguyen100@gmail.com>
2023-06-28 13:54:46 -0400

    handle and display error using state

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlogs] = useState(null); // initiate of blogs
   const [isPending, setIspending] = useState(true); // initiate state of ispending
+  const [error, setError] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -11,26 +12,35 @@ const Home = () => {
   };
 
   useEffect(() => {
-    fetch("http://localhost:3000/blogs") // fetch api from local json server
+    fetch("http://localhost:8000/blogs") // fetch api from local json server
       .then((res) => {
+        console.log(res);
+        if (!res.ok) {
+          throw Error("Could not fetch the data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
-        console.log(data);
         setBlogs(data);
         setIspending(false); // change state of Ispending
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIspending(false);
+        setBlogs(null);
       });
   }, []);
 
   return (
     <div className="home">
-      
-      {isPending && <div>Loading...</div>} {/* condition of loading screen */}
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && (
         <BlogList
           blogs={blogs}
           title={"All blogs"}
-          handleDelete={handleDelete} {/* condition of blogs component */}
+          handleDelete={handleDelete}
         />
       )}
     </div>
