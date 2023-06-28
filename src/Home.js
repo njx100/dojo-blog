d4b4f9c87c9dd518d39712dc2058dbb6cc569154
Dnnguyen <dnnguyen100@gmail.com>
2023-06-28 13:26:35 -0400

    run local json server, fetch data and handle loading screen

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIspending] = useState(true);
+  const [blogs, setBlogs] = useState(null); // initiate of blogs
+  const [isPending, setIspending] = useState(true); // initiate state of ispending
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -11,25 +11,26 @@ const Home = () => {
   };
 
   useEffect(() => {
-    fetch("http://localhost:3000/blogs")
+    fetch("http://localhost:3000/blogs") // fetch api from local json server
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         console.log(data);
         setBlogs(data);
-        setIspending(false);
+        setIspending(false); // change state of Ispending
       });
   }, []);
 
   return (
     <div className="home">
-      {isPending && <div>Loading...</div>}
+      
+      {isPending && <div>Loading...</div>} {/* condition of loading screen */}
       {blogs && (
         <BlogList
           blogs={blogs}
           title={"All blogs"}
-          handleDelete={handleDelete}
+          handleDelete={handleDelete} {/* condition of blogs component */}
         />
       )}
     </div>
